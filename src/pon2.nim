c501ae9023a3098b7f189954fe4baa4312cd1bfa
Keisuke Izumiya <k.izumiya.13@gmail.com>
2024-01-25 04:09:53 +0900

    [refactor] web main

--- src/pon2.nim
+++ src/pon2.nim
@@ -16,109 +16,28 @@
 {.experimental: "strictFuncs".}
 {.experimental: "views".}
 
-when defined(js):
-  when defined(Pon2Worker):
-    import std/[sequtils, strutils, sugar, uri]
-    import ./pon2pkg/apppkg/[editorpermuter]
-    import ./pon2pkg/corepkg/[misc, pair, position]
-    import ./pon2pkg/nazopuyopkg/[nazopuyo, permute, solve]
-    import ./pon2pkg/private/[misc]
-    import ./pon2pkg/private/app/web/[webworker]
+when isMainModule:
+  when defined(js):
+    when defined(Pon2Worker):
+      import ./pon2pkg/private/app/web/[webworker]
+      import ./pon2pkg/private/main/[web]
 
-    proc workerTask(args: seq[string]): tuple[returnCode: WorkerReturnCode,
-                                              messages: seq[string]] =
-      ## Worker's task.
-      if args.len == 0:
-        result.returnCode = Failure
-        result.messages = @["No arguments."]
-        return
-
-      let args2 = args[1..^1]
-      case args[0]
-      of $Solve:
-        if args2.len == 1:
-          result.returnCode = Success
-          args2[0].parseUri.parseNazoPuyos.nazoPuyos.flattenAnd:
-            result.messages = nazoPuyo.solve.mapIt it.toUriQuery Izumiya
-        else:
-          result.returnCode = Failure
-          result.messages = @["Caught invalid number of arguments: " & $args]
-      of $Permute:
-        if args2.len >= 3:
-          let fixMoves =
-            if args2.len == 3: newSeq[Positive](0)
-            else: args2[3..^1].mapIt it.parseSomeInt[:Positive]
-
-          result.returnCode = Success
-          args2[0].parseUri.parseNazoPuyos.nazoPuyos.flattenAnd:
-            let permuteRes = collect:
-              for (pairs, answer) in nazoPuyo.permute(
-                  fixMoves, args2[1].parseBool, args2[2].parseBool):
-                @[pairs.toUriQuery Izumiya, answer.toUriQuery Izumiya]
-            result.messages = permuteRes.concat
-        else:
-          result.returnCode = Failure
-          result.messages = @["Caught invalid number of arguments: " & $args]
-      else:
-        result.returnCode = Failure
-        result.messages = @["Caught invalid task: " & args[0]]
-
-    when isMainModule:
       assignToWorker workerTask
-  else:
-    import std/[options, sequtils, uri]
-    import karax/[karax, karaxdsl, vdom]
-    import ./pon2pkg/apppkg/[editorpermuter, simulator]
-    import ./pon2pkg/corepkg/[environment, misc]
-    import ./pon2pkg/nazopuyopkg/[nazopuyo]
+    else:
+      import std/[sugar]
+      import ./pon2pkg/apppkg/[editorpermuter]
+      import ./pon2pkg/private/main/[web]
+      import karax/[karax]
 
-    var
-      pageInitialized = false
-      globalEditorPermuter: EditorPermuter
+      var
+        pageInitialized = false
+        globalEditorPermuter: EditorPermuter
 
-    proc initEditorPermuterNode(routerData: RouterData): VNode =
-      ## Returns the editor&permuter node.
-      if pageInitialized:
-        return globalEditorPermuter.initEditorPermuterNode
+      setRenderer (routerData: RouterData) => routerData.initEditorPermuterNode(
+        pageInitialized, globalEditorPermuter)
 
-      pageInitialized = true
-      let query =
-        if routerData.queryString == cstring"": ""
-        else: ($routerData.queryString)[1..^1]
-
-      var uri = initUri()
-      uri.scheme = "https"
-      uri.hostname = $Izumiya
-      uri.path = "/pon2/playground/index.html"
-      uri.query = query
-
-      try:
-        let parseRes = uri.parseNazoPuyos
-        parseRes.nazoPuyos.flattenAnd:
-          globalEditorPermuter =
-            if parseRes.positions.isSome:
-              nazoPuyo.initEditorPermuter(parseRes.positions.get, parseRes.mode,
-                                          parseRes.editor)
-            else:
-              nazoPuyo.initEditorPermuter(parseRes.mode, parseRes.editor)
-      except ValueError:
-        try:
-          let parseRes = uri.parseEnvironments
-          parseRes.environments.flattenAnd:
-            globalEditorPermuter =
-              if parseRes.positions.isSome:
-                environment.initEditorPermuter(parseRes.positions.get,
-                                              parseRes.mode, parseRes.editor)
-              else:
-                environment.initEditorPermuter(parseRes.mode, parseRes.editor)
-        except ValueError:
-          return buildHtml(tdiv):
-            text "URL形式エラー"
-
-      result = globalEditorPermuter.initEditorPermuterNode
-
-    when isMainModule:
-      initEditorPermuterNode.setRenderer
+when defined(js):
+  discard
 else:
   import std/[browsers, options, random, sequtils, strformat, strutils, uri]
   import docopt
