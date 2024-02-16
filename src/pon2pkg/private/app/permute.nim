f8b9434333ee9a1054e986c89ebff24b8f13e7ce
Keisuke Izumiya <k.izumiya.13@gmail.com>
2024-02-16 17:33:42 +0900

    [refactor] permute private

--- src/pon2pkg/private/app/permute.nim
+++ src/pon2pkg/private/app/permute.nim
@@ -1,24 +1,28 @@
-## This module implements procedures used in permutation.
+## This module implements helper procedures used in permutation.
 ##
 
 {.experimental: "strictDefs".}
 {.experimental: "strictFuncs".}
 {.experimental: "views".}
 
-import std/[deques, sequtils, sugar]
-import ../../corepkg/[cell, field, pair]
-import ../../nazopuyopkg/[nazopuyo]
+import std/[sequtils, sugar]
+import ../../core/[cell, field, nazopuyo, pair, pairposition]
 
-func allPairsSeq(
-    originalPairs: Pairs, fixMoves: openArray[Positive], allowDouble: bool,
-    allowLastDouble: bool, colorCounts: array[ColorPuyo, Natural],
-    idx: Natural, moveCount: Positive): seq[Pairs] {.inline.} =
-  ## Returns all possible pairs in ascending order that can be obtained by
-  ## permuting puyos contained in the `originalPairs`.
+func allPairsPositionsSeq(
+    originalPairsPositions: PairsPositions,
+    fixMoves: openArray[Positive],
+    allowDouble: bool,
+    allowLastDouble: bool,
+    colorCounts: array[ColorPuyo, Natural],
+    idx: Natural,
+    moveCount: Positive,
+): seq[PairsPositions] {.inline.} =
+  ## Returns all possible pairs (and positions) in ascending order that can be
+  ## obtained by permuting puyos contained in the `originalPairsPositions`.
   # NOTE: Swapped pair sometimes gives a different solution, but this function
   # does not consider it.
   if idx == moveCount:
-    return @[initDeque[Pair](moveCount)]
+    return @[]
 
   result = @[]
   let nowLast = idx == moveCount.pred
@@ -30,9 +34,8 @@ func allPairsSeq(
     var newColorCountsMid = colorCounts
     newColorCountsMid[axis].dec
 
-    for child in axis..ColorPuyo.high:
-      if axis == child and (
-          not allowDouble or (nowLast and not allowLastDouble)):
+    for child in axis .. ColorPuyo.high:
+      if axis == child and (not allowDouble or (nowLast and not allowLastDouble)):
         continue
       if newColorCountsMid[child] == 0:
         continue
@@ -44,28 +47,32 @@ func allPairsSeq(
         nowPairTmp = initPair(axis, child)
         nowPair: Pair
       if idx.succ in fixMoves:
-        if originalPairs[idx] notin {nowPairTmp, nowPairTmp.swapped}:
+        if originalPairsPositions[idx].pair notin {nowPairTmp, nowPairTmp.swapped}:
           continue
 
-        nowPair = originalPairs[idx]
+        nowPair = originalPairsPositions[idx].pair
       else:
         nowPair = nowPairTmp
 
-      result &= originalPairs.allPairsSeq(
-        fixMoves, allowDouble, allowLastDouble, newColorCounts, idx.succ,
-        moveCount).mapIt it.dup addFirst(nowPair)
+      result &=
+        originalPairsPositions.allPairsPositionsSeq(
+          fixMoves, allowDouble, allowLastDouble, newColorCounts, idx.succ, moveCount
+        ).mapIt it.dup insert(nowPair, 0)
 
-func allPairsSeq*[F: TsuField or WaterField](
-    nazo: NazoPuyo[F], fixMoves: openArray[Positive], allowDouble: bool,
-    allowLastDouble: bool): seq[Pairs] {.inline.} =
-  ## Returns all possible pairs in ascending order that can be obtained by
-  ## permuting puyos contained in the pairs.
+func allPairsPositionsSeq*[F: TsuField or WaterField](
+    nazo: NazoPuyo[F],
+    fixMoves: openArray[Positive],
+    allowDouble: bool,
+    allowLastDouble: bool,
+): seq[Pairs] {.inline.} =
+  ## Returns all possible pairs (and positions) in ascending order that can be
+  ## obtained by permuting puyos contained in the pairs.
   # NOTE: Swapped pair sometimes gives a different solution, but this function
   # does not consider it.
   var colorCounts: array[ColorPuyo, Natural] = [0, 0, 0, 0, 0]
   for color in ColorPuyo:
-    colorCounts[color] = nazo.environment.pairs.puyoCount color
+    colorCounts[color] = nazo.puyoPuyo.pairs.puyoCount color
 
-  result = nazo.environment.pairs.allPairsSeq(
-    fixMoves.deduplicate, allowDouble, allowLastDouble, colorCounts, 0,
-    nazo.moveCount)
+  result = nazo.puyoPuyo.pairsPositions.allPairsPositionsSeq(
+    fixMoves.deduplicate, allowDouble, allowLastDouble, colorCounts, 0, nazo.moveCount
+  )
