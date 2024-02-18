fe8cd7d87c8124dde404afe97f6973da66fdf10e
Keisuke Izumiya <k.izumiya.13@gmail.com>
2024-02-18 16:54:42 +0900

    [refactor] simulator misc

--- src/pon2pkg/app/nazopuyo.nim
+++ src/pon2pkg/app/nazopuyo.nim
@@ -5,7 +5,7 @@
 {.experimental: "strictFuncs".}
 {.experimental: "views".}
 
-import ../core/[field, nazopuyo, rule]
+import ../core/[field, nazopuyo, pairposition, requirement, rule]
 
 type NazoPuyoWrap* = object ## Nazo puyo type that accepts all rules.
   tsu*: NazoPuyo[TsuField]
---
+++ src/pon2pkg/private/app/simulator/web/nextpair.nim
@@ -0,0 +1,23 @@
+## This module implements the next pair node.
+##
+
+{.experimental: "strictDefs".}
+{.experimental: "strictFuncs".}
+{.experimental: "views".}
+
+import karax/[karaxdsl, vdom]
+import ../[render]
+import ../../[misc]
+import ../../../../apppkg/[simulator]
+import ../../../../corepkg/[fieldtype]
+
+proc initNextPairNode*(simulator: var Simulator): VNode {.inline.} =
+  ## Returns the next pair node.
+  buildHtml(table):
+    tbody:
+      for idx in -1..1:
+        tr:
+          for col in Column.low..Column.high:
+            td:
+              figure(class = "image is-24x24"):
+                img(src = simulator.nextPairCell(idx, col).cellImageSrc)
