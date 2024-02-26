e8ae28cc83ba61d1585f6cdf5449e04355377c1d
Keisuke Izumiya <k.izumiya.13@gmail.com>
2024-02-27 00:57:19 +0900

    [refactor] native immediate pairs

--- src/pon2pkg/private/app/simulator/native/immediatepairs.nim
+++ src/pon2pkg/private/app/simulator/native/immediatepairs.nim
@@ -9,16 +9,16 @@ import std/[sugar]
 import nigui
 import ./[assets]
 import ../[render]
-import ../../../../apppkg/[misc, simulator]
-import ../../../../corepkg/[cell, misc]
+import ../../../../app/[color, misc, simulator]
+import ../../../../core/[cell]
 
-type ImmediatePairsControl* = ref object of LayoutContainer
-  ## Immediate pairs control.
+type ImmediatePairsControl* = ref object of LayoutContainer ## Immediate pairs control.
   simulator: ref Simulator
   assets: ref Assets
 
-proc cellDrawHandler(control: ImmediatePairsControl, event: DrawEvent,
-                     idx: Natural) {.inline.} =
+proc cellDrawHandler(
+    control: ImmediatePairsControl, event: DrawEvent, idx: Natural
+) {.inline.} =
   ## Draws the cell.
   let canvas = event.control.canvas
 
@@ -30,23 +30,32 @@ proc cellDrawHandler(control: ImmediatePairsControl, event: DrawEvent,
       None
     else:
       case idx
-      of 0..2: Cell.None
-      of 3: control.simulator[].immediateNextPairCell false
-      of 4: control.simulator[].immediateNextPairCell true
-      of 5: Cell.None
-      of 6: control.simulator[].immediateDoubleNextPairCell false
-      of 7: control.simulator[].immediateDoubleNextPairCell true
-      else: Cell.None
+      of 0 .. 2:
+        Cell.None
+      of 3:
+        control.simulator[].immediateNextPairCell false
+      of 4:
+        control.simulator[].immediateNextPairCell true
+      of 5:
+        Cell.None
+      of 6:
+        control.simulator[].immediateDoubleNextPairCell false
+      of 7:
+        control.simulator[].immediateDoubleNextPairCell true
+      else:
+        Cell.None
   canvas.drawImage control.assets[].cellImages[cell]
 
-func initCellDrawHandler(control: ImmediatePairsControl, idx: Natural):
-    (event: DrawEvent) -> void =
+func initCellDrawHandler(
+    control: ImmediatePairsControl, idx: Natural
+): (event: DrawEvent) -> void =
   ## Returns the draw handler.
   # NOTE: cannot inline due to lazy evaluation
   (event: DrawEvent) => control.cellDrawHandler(event, idx)
 
-proc initImmediatePairsControl*(simulator: ref Simulator, assets: ref Assets):
-    ImmediatePairsControl {.inline.} =
+proc initImmediatePairsControl*(
+    simulator: ref Simulator, assets: ref Assets
+): ImmediatePairsControl {.inline.} =
   ## Returns an immediate pairs control.
   result = new ImmediatePairsControl
   result.init
@@ -58,7 +67,7 @@ proc initImmediatePairsControl*(simulator: ref Simulator, assets: ref Assets):
   result.spacing = 0
   result.padding = 0
 
-  for idx in 0..7:
+  for idx in 0 .. 7:
     let cell = newControl()
     result.add cell
 
