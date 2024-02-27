ed378dab01ae58a086b712aa0c1d9de41e1bbbd5
Keisuke Izumiya <k.izumiya.13@gmail.com>
2024-02-27 17:40:01 +0900

    [bug] gui web settings

--- src/pon2pkg/private/app/gui/web/settings.nim
+++ src/pon2pkg/private/app/gui/web/settings.nim
@@ -7,8 +7,8 @@
 
 import std/[sequtils, strformat]
 import karax/[karaxdsl, kbase, kdom, vdom]
-import ../../../../../apppkg/[editorpermuter, simulator]
-import ../../../../../corepkg/[pair, position]
+import ../../../../app/[gui, nazopuyo, simulator]
+import ../../../../core/[pair, pairposition, position]
 
 const
   ParallelCountSelectIdPrefix = "pon2-settings-parallel"
@@ -16,50 +16,64 @@ const
   AllowLastDoubleCheckboxIdPrefix = "pon2-settings-lastdouble"
   FixMovesCheckboxIdPrefix = "pon2-settings-fixmoves"
 
-proc getParallelCount(id: kstring): int {.importjs:
-  &"document.getElementById('{ParallelCountSelectIdPrefix}' + (#)).selectedIndex + 1".}
-  ## Returns the parallel count.
+proc getParallelCount(
+  id: kstring
+): int {.
+  importjs:
+    &"document.getElementById('{ParallelCountSelectIdPrefix}' + (#)).selectedIndex + 1"
+.} ## Returns the parallel count.
 
-proc initEditorSettingsNode*(editorPermuter: var EditorPermuter, id = ""): VNode
-                            {.inline.} =
+proc initEditorSettingsNode*(
+    guiApplication: var GuiApplication, id = ""
+): VNode {.inline.} =
   ## Returns the editor settings node.
+  ## `id` is shared with other node-creating procedures and need to be unique.
   buildHtml(tdiv):
     tdiv(class = "block"):
       button(class = "button is-static px-2"):
         text "並列数"
       tdiv(class = "select"):
         select(id = kstring &"{ParallelCountSelectIdPrefix}{id}"):
-          for count in 1..AllPositions.card:
+          for count in 1 .. AllPositions.card:
             option(selected = count == 6):
               text $count
     tdiv(class = "block"):
-      bold: text "ツモ並べ替え設定"
+      bold:
+        text "ツモ並べ替え設定"
       tdiv(class = "field"):
         tdiv(class = "control"):
           label(class = "checkbox"):
             text "ゾロ"
-            input(id = kstring &"{AllowDoubleCheckboxIdPrefix}{id}",
-                  `type` = "checkbox", checked = true)
+            input(
+              id = kstring &"{AllowDoubleCheckboxIdPrefix}{id}",
+              `type` = "checkbox",
+              checked = true,
+            )
           label(class = "checkbox"):
             text "　最終手ゾロ"
-            input(id = kstring &"{AllowLastDoubleCheckboxIdPrefix}{id}",
-                  `type` = "checkbox")
+            input(
+              id = kstring &"{AllowLastDoubleCheckboxIdPrefix}{id}", `type` = "checkbox"
+            )
           text "　N手目を固定:"
-          for pairIdx in 0..<editorPermuter.simulator[].pairs.len:
+          for pairIdx in 0 ..< guiApplication.simulator[].nazoPuyoWrap.pairsPositions.len:
             label(class = "checkbox"):
               text kstring &"　{pairIdx.succ}"
-              input(id = kstring &"{FixMovesCheckboxIdPrefix}{id}{pairIdx}",
-                    `type` = "checkbox")
+              input(
+                id = kstring &"{FixMovesCheckboxIdPrefix}{id}{pairIdx}",
+                `type` = "checkbox",
+              )
 
-proc getSettings*(id: string, moveCount: Positive): tuple[
-    parallelCount: int, fixMoves: seq[Positive], allowDouble: bool,
-    allowLastDouble: bool] {.inline.} =
-  ## Returns settings.
+proc getSettings*(
+    id: string, moveCount: Positive
+): tuple[
+  parallelCount: int, fixMoves: seq[Positive], allowDouble: bool, allowLastDouble: bool
+] {.inline.} =
+  ## Returns editor settings.
   result.parallelCount = id.getParallelCount
-  result.fixMoves = (1.Positive..moveCount.Positive).toSeq.filterIt(
-    document.getElementById(
-      kstring &"{FixMovesCheckboxIdPrefix}{id}{it.int.pred}").checked)
-  result.allowDouble = document.getElementById(
-    kstring &"{AllowDoubleCheckboxIdPrefix}{id}").checked
-  result.allowLastDouble = document.getElementById(
-    kstring &"{AllowLastDoubleCheckboxIdPrefix}{id}").checked
+  result.fixMoves = (1.Positive .. moveCount.Positive).toSeq.filterIt(
+    document.getElementById(kstring &"{FixMovesCheckboxIdPrefix}{id}{it.int.pred}").checked
+  )
+  result.allowDouble =
+    document.getElementById(kstring &"{AllowDoubleCheckboxIdPrefix}{id}").checked
+  result.allowLastDouble =
+    document.getElementById(kstring &"{AllowLastDoubleCheckboxIdPrefix}{id}").checked
