c53fafb18177ee8e1fcdca719612d56a3e27e3e3
Keisuke Izumiya <k.izumiya.13@gmail.com>
2024-02-27 18:35:54 +0900

    [bug] gui web controller

--- src/pon2pkg/private/app/gui/web/controller.nim
+++ src/pon2pkg/private/app/gui/web/controller.nim
@@ -8,41 +8,51 @@
 import std/[sugar]
 import karax/[karax, karaxdsl, kbase, vdom]
 import ./[settings]
-import ../../../../../apppkg/[editorpermuter, simulator]
-import ../../../../../corepkg/[pair]
-import ../../../../../nazopuyopkg/[nazopuyo]
+import ../../../../app/[gui, nazopuyo, simulator]
+import ../../../../core/[nazopuyo, pairposition]
 
-proc initEditorControllerNode*(editorPermuter: var EditorPermuter, id = ""):
-    VNode {.inline.} =
+proc initEditorControllerNode*(
+    guiApplication: var GuiApplication, id = ""
+): VNode {.inline.} =
   ## Returns the editor controller node.
+  ## `id` is shared with other node-creating procedures and need to be unique.
   let
-    workerRunning = editorPermuter.solving or editorPermuter.permuting
-    workerDisable = workerRunning or editorPermuter.simulator[].pairs.len == 0
+    workerRunning = guiApplication.solving or guiApplication.permuting
+    workerDisable =
+      workerRunning or guiApplication.simulator[].nazoPuyoWrap.pairsPositions.len == 0
 
     focusButtonClass =
-      if editorPermuter.focusEditor: kstring"button"
-      else: kstring"button is-selected is-primary"
+      if guiApplication.focusEditor:
+        kstring"button"
+      else:
+        kstring"button is-selected is-primary"
     solveButtonClass =
-      if editorPermuter.solving: kstring"button is-loading"
-      else: kstring"button"
+      if guiApplication.solving:
+        kstring"button is-loading"
+      else:
+        kstring"button"
     permuteButtonClass =
-      if editorPermuter.permuting: kstring"button is-loading"
-      else: kstring"button"
+      if guiApplication.permuting:
+        kstring"button is-loading"
+      else:
+        kstring"button"
 
-  proc permuteHandler =
-    editorPermuter.simulator[].withNazoPuyo:
-      let (_, fixMoves, allowDouble, allowLastDouble) = getSettings(
-        id, nazoPuyo.moveCount)
-      editorPermuter.permute fixMoves, allowDouble, allowLastDouble
+  proc permuteHandler() =
+    guiApplication.simulator[].nazoPuyoWrap.flattenAnd:
+      let (_, fixMoves, allowDouble, allowLastDouble) =
+        getSettings(id, nazoPuyo.moveCount)
+      guiApplication.permute fixMoves, allowDouble, allowLastDouble
 
   result = buildHtml(tdiv(class = "buttons")):
-    button(class = solveButtonClass, disabled = workerDisable,
-           onclick = () => (
-            editorPermuter.solve getSettings(id, 1).parallelCount)):
+    button(
+      class = solveButtonClass,
+      disabled = workerDisable,
+      onclick = () => (guiApplication.solve getSettings(id, 1).parallelCount),
+    ):
       text "解探索"
-    button(class = permuteButtonClass, disabled = workerDisable,
-           onclick = permuteHandler):
+    button(
+      class = permuteButtonClass, disabled = workerDisable, onclick = permuteHandler
+    ):
       text "ツモ並べ替え"
-    button(class = focusButtonClass,
-           onclick = () => editorPermuter.toggleFocus):
+    button(class = focusButtonClass, onclick = () => guiApplication.toggleFocus):
       text "シミュを操作"
