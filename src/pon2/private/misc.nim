469ff0cb755fd47ff8d0153cfed906127ab24057
Keisuke Izumiya <unstitch_serpent852@simplelogin.com>
2024-10-09 07:08:13 +0900

    [bug] joinPath2

--- src/pon2/private/misc.nim
+++ src/pon2/private/misc.nim
@@ -34,9 +34,11 @@ func joinPath2(head, tail: string): string {.inline.} =
 
 proc getPon2RootDir(): string {.inline.} =
   ## Returns the root directory of Pon2.
-  let (head, tail) = currentSourcePath().splitPath2.head.splitPath2.head.splitPath2
+  let
+    (head, tail) = currentSourcePath().splitPath2.head.splitPath2.head.splitPath2
+    (head2, tail2) = head.splitPath2
 
-  result = if tail == "src": head.splitPath2.head else: head
+  result = if tail2 == "src": head2 else: head
 
 const
   Pon2RootDir* = getPon2RootDir()
