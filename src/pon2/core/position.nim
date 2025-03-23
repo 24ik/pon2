0196a0e346a6781ab5672e7c362d455f1f55717a
Keisuke Izumiya <unstitch_serpent852@simplelogin.com>
2025-03-24 05:46:36 +0900

    [breaking] pos

--- src/pon2/core/position.nim
+++ src/pon2/core/position.nim
@@ -1,27 +1,27 @@
 ## This module implements positions.
 ##
 
-{.experimental: "inferGenericTypes".}
-{.experimental: "notnil".}
-{.experimental: "strictCaseObjects".}
+{.push raises: [].}
 {.experimental: "strictDefs".}
 {.experimental: "strictFuncs".}
 {.experimental: "views".}
 
-import std/[sugar, tables]
-import ./[fieldtype, fqdn]
+import std/[strformat, sugar]
+import results
+import stew/[assign2]
+import stew/shims/[tables]
+import ./[common, fqdn]
 
 type
-  Direction* {.pure.} = enum
-    ## Child-puyo's direction seen from the axis-puyo.
+  Dir* {.pure.} = enum
+    ## Rotor-puyo's direction seen from the pivot-puyo.
     Up = "^"
     Right = ">"
     Down = "v"
     Left = "<"
 
-  Position* {.pure.} = enum
+  Pos* {.pure.} = enum
     ## The location where a pair is put.
-    None = ""
     Up0 = "1N"
     Up1 = "2N"
     Up2 = "3N"
@@ -33,175 +33,250 @@ type
     Right2 = "34"
     Right3 = "45"
     Right4 = "56"
-    Down0 = "1F"
-    Down1 = "2F"
-    Down2 = "3F"
-    Down3 = "4F"
-    Down4 = "5F"
-    Down5 = "6F"
+    Down0 = "1S"
+    Down1 = "2S"
+    Down2 = "3S"
+    Down3 = "4S"
+    Down4 = "5S"
+    Down5 = "6S"
     Left1 = "21"
     Left2 = "32"
     Left3 = "43"
     Left4 = "54"
     Left5 = "65"
 
+  OptPos* = Opt[Pos]
+
 const
-  AllPositions* = {Up0 .. Left5}
-  AllDoublePositions* = {Up0 .. Right4}
+  NonePos* = Opt[Pos].err()
+  AllDblPoses* = {Up0 .. Right4}
 
 # ------------------------------------------------
 # Constructor
 # ------------------------------------------------
 
 const
-  StartPositions: array[Direction, Position] = [Up0, Right0, Down0, Left1]
-  StartColumns: array[Direction, Column] = [0, 0, 0, 1]
+  StartPoses: array[Dir, Pos] = [Up0, Right0, Down0, Left1]
+  StartCols: array[Dir, Col] = [Col0, Col0, Col0, Col1]
 
-func initPosition*(axisCol: Column, childDir: Direction): Position {.inline.} =
-  ## Returns a new position.
-  StartPositions[childDir].succ axisCol - StartColumns[childDir]
+func init*(T: type Pos, pivotCol: Col, rotorDir: Dir): T {.inline.} =
+  StartPoses[rotorDir].succ pivotCol.ord - StartCols[rotorDir].ord
 
 # ------------------------------------------------
 # Property
 # ------------------------------------------------
 
-func initPosToAxisCol(): array[Position, Column] {.inline.} =
-  ## Returns `PosToAxisCol`.
-  result[None] = Column.low
+func initPivotCols(): array[Pos, Col] {.inline.} =
+  ## Returns `PivotCols`.
+  var pivotCols {.noinit.}: array[Pos, Col]
   for pos in Up0 .. Up5:
-    result[pos] = StartColumns[Up].succ pos.ord - StartPositions[Up].ord
+    pivotCols[pos] = StartCols[Up].succ pos.ord - StartPoses[Up].ord
   for pos in Right0 .. Right4:
-    result[pos] = StartColumns[Right].succ pos.ord - StartPositions[Right].ord
+    pivotCols[pos] = StartCols[Right].succ pos.ord - StartPoses[Right].ord
   for pos in Down0 .. Down5:
-    result[pos] = StartColumns[Down].succ pos.ord - StartPositions[Down].ord
+    pivotCols[pos] = StartCols[Down].succ pos.ord - StartPoses[Down].ord
   for pos in Left1 .. Left5:
-    result[pos] = StartColumns[Left].succ pos.ord - StartPositions[Left].ord
+    pivotCols[pos] = StartCols[Left].succ pos.ord - StartPoses[Left].ord
 
-const PosToAxisCol = initPosToAxisCol()
+  pivotCols
 
-func initPosToChildCol(): array[Position, Column] {.inline.} =
-  ## Returns `PosToChildCol`.
-  result[None] = Column.low
+const PivotCols = initPivotCols()
+
+func initRotorCols(): array[Pos, Col] {.inline.} =
+  ## Returns `RotorCols`.
+  var rotorCols {.noinit.}: array[Pos, Col]
   for pos in Up0 .. Up5:
-    result[pos] = PosToAxisCol[pos]
+    rotorCols[pos] = PivotCols[pos]
   for pos in Right0 .. Right4:
-    result[pos] = PosToAxisCol[pos].succ
+    rotorCols[pos] = PivotCols[pos].succ
   for pos in Down0 .. Down5:
-    result[pos] = PosToAxisCol[pos]
+    rotorCols[pos] = PivotCols[pos]
   for pos in Left1 .. Left5:
-    result[pos] = PosToAxisCol[pos].pred
+    rotorCols[pos] = PivotCols[pos].pred
 
-const PosToChildCol = initPosToChildCol()
+  rotorCols
 
-func initPosToChildDir(): array[Position, Direction] {.inline.} =
-  ## Returns `PosToChildDir`.
-  result[None] = Direction.low
+func initRotorDirs(): array[Pos, Dir] {.inline.} =
+  ## Returns `RotorDirs`.
+  var rotorDirs {.noinit.}: array[Pos, Dir]
   for pos in Up0 .. Up5:
-    result[pos] = Up
+    rotorDirs[pos] = Up
   for pos in Right0 .. Right4:
-    result[pos] = Right
+    rotorDirs[pos] = Right
   for pos in Down0 .. Down5:
-    result[pos] = Down
+    rotorDirs[pos] = Down
   for pos in Left1 .. Left5:
-    result[pos] = Left
+    rotorDirs[pos] = Left
 
-const PosToChildDir = initPosToChildDir()
+  rotorDirs
 
-func axisColumn*(self: Position): Column {.inline.} = ## Returns the axis-puyo's column.
-  PosToAxisCol[self]
+const
+  RotorCols = initRotorCols()
+  RotorDirs = initRotorDirs()
 
-func childColumn*(self: Position): Column {.inline.} =
-  ## Returns the child-puyo's column.
-  PosToChildCol[self]
+func pivotCol*(self: Pos): Col {.inline.} = ## Returns the pivot-puyo's column.
+  PivotCols[self]
 
-func childDirection*(self: Position): Direction {.inline.} =
-  ## Returns the child-puyo's direction.
-  PosToChildDir[self]
+func rotorCol*(self: Pos): Col {.inline.} =
+  ## Returns the rotor-puyo's column.
+  RotorCols[self]
+
+func rotorDir*(self: Pos): Dir {.inline.} =
+  ## Returns the rotor-puyo's direction.
+  RotorDirs[self]
 
 # ------------------------------------------------
 # Move
 # ------------------------------------------------
 
+func initRightPoses(): array[Pos, Pos] {.inline.} =
+  ## Returns `RightPoses`.
+  var rightPoses {.noinit.}: array[Pos, Pos]
+  for pos in Pos:
+    let
+      pivotCol = pos.pivotCol
+      newPivotCol = if pivotCol == Col.high: Col.high else: pivotCol.succ
+
+    rightPoses[pos] = Pos.init(newPivotCol, pos.rotorDir)
+
+  rightPoses
+
+func initLeftPoses(): array[Pos, Pos] {.inline.} =
+  ## Returns `LeftPoses`.
+  var leftPoses {.noinit.}: array[Pos, Pos]
+  for pos in Pos:
+    let
+      pivotCol = pos.pivotCol
+      newPivotCol = if pivotCol == Col.low: Col.low else: pivotCol.pred
+
+    leftPoses[pos] = Pos.init(newPivotCol, pos.rotorDir)
+
+  leftPoses
+
 const
-  RightPositions: array[Position, Position] = [
-    None, Up1, Up2, Up3, Up4, Up5, Up5, Right1, Right2, Right3, Right4, Right4, Down1,
-    Down2, Down3, Down4, Down5, Down5, Left2, Left3, Left4, Left5, Left5,
-  ]
-  LeftPositions: array[Position, Position] = [
-    None, Up0, Up0, Up1, Up2, Up3, Up4, Right0, Right0, Right1, Right2, Right3, Down0,
-    Down0, Down1, Down2, Down3, Down4, Left1, Left1, Left2, Left3, Left4,
-  ]
+  RightPoses = initRightPoses()
+  LeftPoses = initLeftPoses()
 
-func movedRight*(self: Position): Position {.inline.} =
+func movedRight*(self: Pos): Pos {.inline.} =
   ## Returns the position moved rightward.
-  RightPositions[self]
+  RightPoses[self]
 
-func movedLeft*(self: Position): Position {.inline.} =
+func movedLeft*(self: Pos): Pos {.inline.} =
   ## Returns the position moved leftward.
-  LeftPositions[self]
+  LeftPoses[self]
 
-func moveRight*(self: var Position) {.inline.} = ## Moves the position rightward.
-  self = self.movedRight
+func moveRight*(self: var Pos) {.inline.} = ## Moves the position rightward.
+  self.assign self.movedRight
 
-func moveLeft*(self: var Position) {.inline.} = ## Moves the position leftward.
-  self = self.movedLeft
+func moveLeft*(self: var Pos) {.inline.} = ## Moves the position leftward.
+  self.assign self.movedLeft
 
 # ------------------------------------------------
 # Rotate
 # ------------------------------------------------
 
+func initRightRotatePoses(): array[Pos, Pos] {.inline.} =
+  ## Returns `RightRotatePoses`.
+  var rightRotatePoses {.noinit.}: array[Pos, Pos]
+  for pos in Pos:
+    let
+      pivotCol = pos.pivotCol
+      rotorDir = pos.rotorDir
+      newPivotCol =
+        if pivotCol == Col.high and rotorDir == Up:
+          pivotCol.pred
+        elif pivotCol == Col.low and rotorDir == Down:
+          pivotCol.succ
+        else:
+          pivotCol
+      newRotorDir = if rotorDir == Dir.high: Dir.low else: rotorDir.succ
+
+    rightRotatePoses[pos] = Pos.init(newPivotCol, newRotorDir)
+
+  rightRotatePoses
+
+func initLeftRotatePoses(): array[Pos, Pos] {.inline.} =
+  ## Returns `LeftRotatePoses`.
+  var leftRotatePoses {.noinit.}: array[Pos, Pos]
+  for pos in Pos:
+    let
+      pivotCol = pos.pivotCol
+      rotorDir = pos.rotorDir
+      newPivotCol =
+        if pivotCol == Col.high and rotorDir == Down:
+          pivotCol.pred
+        elif pivotCol == Col.low and rotorDir == Up:
+          pivotCol.succ
+        else:
+          pivotCol
+      newRotorDir = if rotorDir == Dir.low: Dir.high else: rotorDir.pred
+
+    leftRotatePoses[pos] = Pos.init(newPivotCol, newRotorDir)
+
+  leftRotatePoses
+
 const
-  RightRotatePositions: array[Position, Position] = [
-    None, Right0, Right1, Right2, Right3, Right4, Right4, Down0, Down1, Down2, Down3,
-    Down4, Left1, Left1, Left2, Left3, Left4, Left5, Up1, Up2, Up3, Up4, Up5,
-  ]
-  LeftRotatePositions: array[Position, Position] = [
-    None, Left1, Left1, Left2, Left3, Left4, Left5, Up0, Up1, Up2, Up3, Up4, Right0,
-    Right1, Right2, Right3, Right4, Right4, Down1, Down2, Down3, Down4, Down5,
-  ]
+  RightRotatePoses = initRightRotatePoses()
+  LeftRotatePoses = initLeftRotatePoses()
 
-func rotatedRight*(self: Position): Position {.inline.} =
+func rotatedRight*(self: Pos): Pos {.inline.} =
   ## Returns the position rotated right (clockwise).
-  RightRotatePositions[self]
+  RightRotatePoses[self]
 
-func rotatedLeft*(self: Position): Position {.inline.} =
+func rotatedLeft*(self: Pos): Pos {.inline.} =
   ## Returns the position rotated left (counterclockwise).
-  LeftRotatePositions[self]
+  LeftRotatePoses[self]
 
-func rotateRight*(self: var Position) {.inline.} = ## Rotates the position right.
-  self = self.rotatedRight
+func rotateRight*(self: var Pos) {.inline.} =
+  ## Rotates the position right (clockwise).
+  self.assign self.rotatedRight
 
-func rotateLeft*(self: var Position) {.inline.} = ## Rotates the position left.
-  self = self.rotatedLeft
+func rotateLeft*(self: var Pos) {.inline.} =
+  ## Rotates the position left (counterclockwise).
+  self.assign self.rotatedLeft
 
 # ------------------------------------------------
 # Position <-> string
 # ------------------------------------------------
 
-const StrToPos = collect:
-  for pos in Position:
-    {$pos: pos}
+const
+  NonePosStr = ""
+  StrToPos = collect:
+    for pos in Pos:
+      {$pos: pos}
 
-func parsePosition*(str: string): Position {.inline.} =
+func `$`*(self: OptPos): string {.inline.} =
+  if self.isOk:
+    $self.value
+  else:
+    NonePosStr
+
+func parsePos*(str: string): Result[Pos, string] {.inline.} =
   ## Returns the position converted from the string representation.
-  ## If the string is invalid, `ValueError` is raised.
-  try:
-    result = StrToPos[str]
-  except KeyError:
-    result = Position.low # HACK: dummy to suppress warning
-    raise newException(ValueError, "Invalid position: " & str)
+  if str in StrToPos:
+    Result[Pos, string].ok StrToPos[str]
+  else:
+    Result[Pos, string].err "Invalid pos: {str}".fmt
+
+func parseOptPos*(str: string): Result[OptPos, string] {.inline.} =
+  ## Returns the optional position converted from the string representation.
+  if str == NonePosStr:
+    Result[OptPos, string].ok NonePos
+  else:
+    Result[OptPos, string].ok OptPos.ok ?str.parsePos
 
 # ------------------------------------------------
 # Position <-> URI
 # ------------------------------------------------
 
 const
-  PosToIshikawaUri = "102468acegikoqsuwyCEGIK"
+  PosToIshikawaUri = "02468acegikoqsuwyCEGIK"
+  NonePosIshikawaUri = "1"
   IshikawaUriToPos = collect:
-    for pos in Position:
+    for pos in Pos:
       {$PosToIshikawaUri[pos.ord]: pos}
 
-func toUriQuery*(self: Position, fqdn = Pon2): string {.inline.} =
+func toUriQuery*(self: Pos, fqdn = Pon2): string {.inline.} =
   ## Returns the URI query converted from the position.
   case fqdn
   of Pon2:
@@ -209,15 +284,35 @@ func toUriQuery*(self: Position, fqdn = Pon2): string {.inline.} =
   of Ishikawa, Ips:
     $PosToIshikawaUri[self.ord]
 
-func parsePosition*(query: string, fqdn: IdeFqdn): Position {.inline.} =
-  ## Returns the position converted from the URI query.
-  ## If the query is invalid, `ValueError` is raised.
+func toUriQuery*(self: OptPos, fqdn = Pon2): string {.inline.} =
+  ## Returns the URI query converted from the optional position.
   case fqdn
   of Pon2:
-    result = query.parsePosition
+    $self
   of Ishikawa, Ips:
-    try:
-      result = IshikawaUriToPos[query]
-    except KeyError:
-      result = Position.low # HACK: dummy to suppress warning
-      raise newException(ValueError, "Invalid position: " & query)
+    if self.isOk:
+      $PosToIshikawaUri[self.value.ord]
+    else:
+      NonePosIshikawaUri
+
+func parsePos*(query: string, fqdn: IdeFqdn): Result[Pos, string] {.inline.} =
+  ## Returns the position converted from the URI query.
+  case fqdn
+  of Pon2:
+    query.parsePos
+  of Ishikawa, Ips:
+    if query in IshikawaUriToPos:
+      Result[Pos, string].ok IshikawaUriToPos[query]
+    else:
+      Result[Pos, string].err "Invalid pos: {query}".fmt
+
+func parseOptPos*(query: string, fqdn: IdeFqdn): Result[OptPos, string] {.inline.} =
+  ## Returns the optional position converted from the URI query.
+  case fqdn
+  of Pon2:
+    query.parseOptPos
+  of Ishikawa, Ips:
+    if query == NonePosIshikawaUri:
+      Result[OptPos, string].ok NonePos
+    else:
+      Result[OptPos, string].ok ?query.parsePos fqdn
