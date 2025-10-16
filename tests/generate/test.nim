621645ba2ac0a3fe521e725f732a140a927b7bf2
Keisuke Izumiya <unstitch_serpent852@simplelogin.com>
2025-10-17 03:46:02 +0900

    [test] generate

--- tests/generate/test.nim
+++ tests/generate/test.nim
@@ -5,132 +5,7 @@
 
 import std/[random, sequtils, sugar, unittest]
 import ../../src/pon2/[core]
-import ../../src/pon2/app/[generate, nazopuyowrap]
-import ../../src/pon2/private/[math2]
-import ../../src/pon2/private/app/[generate]
-
-# ------------------------------------------------
-# private
-# ------------------------------------------------
-
-block: # split
-  var rng = 123.initRand
-
-  block: # split by chunkCnt
-    block: # allow zero chunk
-      block:
-        let
-          total = 10
-          chunkCnt = 5
-          res = rng.split(total, chunkCnt, allowZeroChunk = true)
-
-        check res.isOk
-        check res.unsafeValue.sum2 == total
-        check res.unsafeValue.len == chunkCnt
-        check res.unsafeValue.allIt it >= 0
-
-      block: # corner case
-        let
-          total = 10
-          chunkCnt = 5
-
-        check rng.split(total, 0, allowZeroChunk = true).isErr
-        check rng.split(total, 1, allowZeroChunk = true) == Res[seq[int]].ok @[total]
-        check rng.split(-1, chunkCnt, allowZeroChunk = true).isErr
-        check rng.split(0, chunkCnt, allowZeroChunk = true) ==
-          Res[seq[int]].ok 0.repeat chunkCnt
-
-    block: # not allow zero chunk
-      block:
-        let
-          total = 10
-          chunkCnt = 5
-          res = rng.split(total, chunkCnt, allowZeroChunk = false)
-
-        check res.isOk
-        check res.unsafeValue.sum2 == total
-        check res.unsafeValue.len == chunkCnt
-        check res.unsafeValue.allIt it > 0
-
-      block: # corner case
-        let
-          total = 10
-          chunkCnt = 5
-
-        check rng.split(total, 0, allowZeroChunk = false).isErr
-        check rng.split(total, 1, allowZeroChunk = false) == Res[seq[int]].ok @[total]
-        check rng.split(-1, chunkCnt, allowZeroChunk = false).isErr
-        check rng.split(0, chunkCnt, allowZeroChunk = false).isErr
-
-  block: # split by weights
-    block:
-      let
-        total = 5
-        weights = [0, 1, 2]
-        res = rng.split(total, weights)
-
-      check res.isOk
-      check res.unsafeValue.sum2 == total
-      check res.unsafeValue.len == weights.len
-      check res.unsafeValue.allIt it >= 0
-
-    block: # all zero
-      let
-        total = 5
-        weights = [0, 0, 0]
-        res = rng.split(total, weights)
-
-      check res.isOk
-      check res.unsafeValue.sum2 == total
-      check res.unsafeValue.len == weights.len
-      check res.unsafeValue.allIt it >= 0
-
-    block: # corner case
-      let
-        total = 5
-        weights = [0, 1, 2]
-
-      check rng.split(total, newSeq[int]()).isErr
-      check rng.split(total, [0]) == Res[seq[int]].ok @[total]
-      check rng.split(-1, weights).isErr
-      check rng.split(0, weights) == Res[seq[int]].ok 0.repeat weights.len
-
-  block: # split by positives
-    block:
-      let
-        total = 5
-        positives = [true, false, true]
-        res = rng.split(total, positives)
-
-      check res.isOk
-      check res.unsafeValue.sum2 == total
-      check res.unsafeValue.len == positives.len
-      for i, pos in positives:
-        if pos:
-          check res.unsafeValue[i] > 0
-        else:
-          check res.unsafeValue[i] == 0
-
-    block: # all false
-      let
-        total = 5
-        positives = [false, false, false]
-        res = rng.split(total, positives)
-
-      check res.isOk
-      check res.unsafeValue.sum2 == total
-      check res.unsafeValue.len == positives.len
-      check res.unsafeValue.allIt it >= 0
-
-    block: # corner case
-      let
-        total = 5
-        positives = [true, false, true]
-
-      check rng.split(total, newSeq[bool]()).isErr
-      check rng.split(total, [true]) == Res[seq[int]].ok @[total]
-      check rng.split(-1, positives).isErr
-      check rng.split(0, positives).isErr
+import ../../src/pon2/app/[generate, nazopuyowrap, solve]
 
 # ------------------------------------------------
 # Generate
@@ -249,3 +124,13 @@ block: # generate
     if not allowHardStep:
       check nazo.puyoPuyo.steps.toSeq.all (step) =>
         step.kind != StepKind.Garbages or not step.dropHard
+
+    let ans = nazo.solve
+    check ans.len == 1
+    check ans[0].len == moveCnt
+    for stepIdx, step in nazo.puyoPuyo.steps:
+      case step.kind
+      of PairPlacement:
+        check step.optPlacement == ans[0][stepIdx]
+      else:
+        check ans[0][stepIdx].isErr
