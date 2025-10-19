c26ed686df78bd13f979de7ce0f95cf7d144ce0f
Keisuke Izumiya <unstitch_serpent852@simplelogin.com>
2025-10-19 19:02:31 +0900

    [enhancement] ide.nim

---
+++ src/pon2/gui/ide.nim
@@ -0,0 +1,17 @@
+## This module implements IDE views.
+##
+## Submodule Documentations:
+## - [ctrl](./ide/ctrl.html)
+## - [ide](./ide/ide.html)
+## - [pagination](./ide/pagination.html)
+## - [setting](./ide/setting.html)
+##
+
+{.push raises: [].}
+{.experimental: "strictDefs".}
+{.experimental: "strictFuncs".}
+{.experimental: "views".}
+
+import ./ide/[ctrl, ide, pagination, setting]
+
+export ctrl, ide, pagination, setting
