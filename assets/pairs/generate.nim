c58627d19bb551f66efc2232c940cc796824a841
Keisuke Izumiya <78471944+24ik@users.noreply.github.com>
2025-10-21 04:34:14 +0000

    add 0.23 to repo root

---
+++ assets/pairs/generate.nim
@@ -0,0 +1,41 @@
+## This module implements the generation of pairs assets.
+##
+
+{.experimental: "inferGenericTypes".}
+{.experimental: "notnil".}
+{.experimental: "strictCaseObjects".}
+{.experimental: "strictDefs".}
+{.experimental: "strictFuncs".}
+{.experimental: "views".}
+
+import std/[os, sequtils, strutils, sugar]
+
+const
+  PairsCount = 65536
+  PairCount = 128
+  LineLen = PairCount * 2
+
+when isMainModule:
+  let
+    rawPath = currentSourcePath() /../ "raw.txt"
+    rawLines = rawPath.readFile.splitLines
+  doAssert rawLines.len == PairsCount
+  doAssert rawLines.allIt it.len == LineLen
+
+  # marathon; axis and child swapped
+  block:
+    let pairStrs = collect:
+      for line in rawLines:
+        for i in countup(0, LineLen.pred, 2):
+          line[i.succ] & line[i]
+    doAssert pairStrs.len == PairCount * PairsCount
+    doAssert pairStrs.allIt it.len == 2
+
+    let lines = collect:
+      for pairsIdx in 0 ..< PairsCount:
+        join pairStrs[pairsIdx * PairCount ..< pairsIdx.succ * PairCount]
+    doAssert lines.len == PairsCount
+    doAssert lines.allIt it.len == LineLen
+
+    let dstPath = currentSourcePath() /../ "swap.txt"
+    dstPath.writeFile lines.join "\n"
