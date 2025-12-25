5551105e1f98d91db2a6b54173f5d55b6079efb0
Keisuke Izumiya <78471944+24ik@users.noreply.github.com>
2025-12-25 21:12:31 +0900

    [enhancement] gui message

--- src/pon2/gui/simulator/msg.nim
+++ src/pon2/gui/simulator/msg.nim
@@ -11,10 +11,11 @@
 # ------------------------------------------------
 
 when defined(js) or defined(nimsuggest):
+  import std/[sequtils]
   import karax/[karaxdsl, vdom]
   import ../[helper]
   import ../../[app]
-  import ../../private/[arrayutils, assign, gui, math]
+  import ../../private/[arrayutils, assign, gui, math, strutils]
 
   {.push warning[UnusedImport]: off.}
   import karax/[kbase]
@@ -24,26 +25,7 @@ when defined(js) or defined(nimsuggest):
 
   const ShowNoticeCount = 6
 
-  proc txtMsg[S: Simulator or Studio or Marathon](
-      self: ref S, helper: VNodeHelper
-  ): string =
-    ## Returns the text message.
-    if self.derefSimulator(helper).nazoPuyo.goal == NoneGoal:
-      if self.derefSimulator(helper).state == Stable and
-          self.derefSimulator(helper).nazoPuyo.puyoPuyo.field.isDead:
-        $MarkResult.Dead
-      else:
-        ""
-    else:
-      $helper.simulator.markResult
-
-  proc score[S: Simulator or Studio or Marathon](
-      self: ref S, helper: VNodeHelper
-  ): int =
-    ## Returns the score.
-    self.derefSimulator(helper).moveResult.score.unsafeValue
-
-  proc noticeCounts[S: Simulator or Studio or Marathon](
+  func noticeCounts[S: Simulator or Studio or Marathon](
       self: ref S, helper: VNodeHelper, score: int
   ): array[Notice, int] =
     ## Returns the numbers of notice garbages.
@@ -62,17 +44,113 @@ when defined(js) or defined(nimsuggest):
 
     counts
 
+  func goalMsg[S: Simulator or Studio or Marathon](
+      self: ref S, helper: VNodeHelper, goal: Goal
+  ): string =
+    ## Returns the goal message.
+    let
+      mainMsg =
+        if goal.mainOpt.isOk:
+          let
+            main = goal.mainOpt.unsafeValue
+            moveResult = self.derefSimulator(helper).moveResult
+
+          case main.kind
+          of Chain:
+            "{moveResult.chainCount}連鎖".fmt
+          of GoalKind.Color:
+            let colorCount =
+              if moveResult.chainCount == 0:
+                0
+              else:
+                moveResult.colorsSeq[^1].card
+            "{colorCount}色".fmt
+          of Count:
+            let count =
+              if moveResult.chainCount == 0:
+                0
+              else:
+                case main.color
+                of All:
+                  moveResult.puyoCounts[^1]
+                of Nuisance:
+                  moveResult.nuisancePuyoCounts[^1]
+                of Colored:
+                  moveResult.coloredPuyoCounts[^1]
+                else:
+                  moveResult.cellCounts(main.color.ord.Cell)[^1]
+            "{count}個".fmt
+          of Place:
+            let placeCount =
+              if moveResult.chainCount == 0:
+                0
+              else:
+                case main.color
+                of All, Nuisance, Colored:
+                  moveResult.placeCounts.unsafeValue[^1]
+                else:
+                  moveResult.placeCounts(main.color.ord.Cell).unsafeValue[^1]
+            "{placeCount}箇所".fmt
+          of Connection:
+            let connectionCount =
+              if moveResult.chainCount == 0:
+                0
+              else:
+                case main.color
+                of All, Nuisance, Colored:
+                  moveResult.fullPopCountsOpt.unsafeValue[^1].concat.max
+                else:
+                  moveResult.fullPopCountsOpt.unsafeValue[^1][main.color.ord.Cell].max
+            "{connectionCount}箇所".fmt
+          of AccumColor:
+            let colorCount = ColoredPuyos.countIt moveResult.popCounts[it] > 0
+            "{colorCount}色".fmt
+          of AccumCount:
+            let count =
+              case main.color
+              of All:
+                moveResult.puyoCount
+              of Nuisance:
+                moveResult.nuisancePuyoCount
+              of Colored:
+                moveResult.coloredPuyoCount
+              else:
+                moveResult.cellCount main.color.ord.Cell
+            "{count}個".fmt
+        else:
+          ""
+      clearColorMsg =
+        if goal.clearColorOpt.isOk:
+          let
+            clearColor = goal.clearColorOpt.unsafeValue
+            count =
+              case clearColor
+              of All:
+                self.derefSimulator(helper).nazoPuyo.puyoPuyo.field.puyoCount
+              of Nuisance:
+                self.derefSimulator(helper).nazoPuyo.puyoPuyo.field.nuisancePuyoCount
+              of Colored:
+                self.derefSimulator(helper).nazoPuyo.puyoPuyo.field.coloredPuyoCount
+              else:
+                self.derefSimulator(helper).nazoPuyo.puyoPuyo.field.cellCount clearColor.ord.Cell
+
+          "残り{count}個".fmt
+        else:
+          ""
+
+    "{mainMsg}&{clearColorMsg}".fmt.strip(chars = {'&'})
+
   proc toMsgVNode*[S: Simulator or Studio or Marathon](
       self: ref S, helper: VNodeHelper
   ): VNode =
     ## Returns the message node.
     let
-      score = self.score helper
+      score = self.derefSimulator(helper).moveResult.score.unsafeValue
       noticeCounts = self.noticeCounts(helper, score)
-      showNotice = self.derefSimulator(helper).nazoPuyo.goal == NoneGoal
+      goal = self.derefSimulator(helper).nazoPuyo.goal
 
     buildHtml tdiv:
-      if showNotice:
+      if goal == NoneGoal or not goal.isSupported:
         table:
           tbody:
             tr:
@@ -88,5 +166,13 @@ when defined(js) or defined(nimsuggest):
                     img(src = Cell.None.cellImgSrc)
               td:
                 tdiv(class = "is-size-7"):
-                  text (" " & $score).kstring
-      text self.txtMsg(helper).kstring
+                  text " {score}".fmt.kstring
+        if self.derefSimulator(helper).state == Stable and
+            self.derefSimulator(helper).nazoPuyo.puyoPuyo.field.isDead:
+          tdiv(class = "is-size-7"):
+            text ($Dead).kstring
+      else:
+        p:
+          text self.goalMsg(helper, goal).kstring
+        p:
+          text ($helper.simulator.markResult).kstring
