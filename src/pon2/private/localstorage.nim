bd1177d054b7bcc256dae0d1d36e95e2b59f31d4
Keisuke Izumiya <78471944+24ik@users.noreply.github.com>
2026-01-07 04:19:41 +0900

    [refactor] add localstorage

---
+++ src/pon2/private/localstorage.nim
@@ -0,0 +1,40 @@
+## This module implements a local storage.
+##
+
+{.push raises: [].}
+{.experimental: "strictDefs".}
+{.experimental: "strictFuncs".}
+{.experimental: "views".}
+
+import std/[strformat]
+import karax/[localstorage]
+import ../[utils]
+
+export utils
+
+type LocalStorage* = object ## Local storage.
+
+func init*(T: type LocalStorage): T {.inline, noinit.} =
+  LocalStorage()
+
+proc pathPrefixAdded(key: string): cstring {.inline, noinit.} =
+  ## Returns the key with the path prefix added.
+  "{window.location.pathname}-{key}".fmt.cstring
+
+proc contains*(self: LocalStorage, key: string): bool {.inline, noinit.} =
+  key.pathPrefixAdded.hasItem
+
+proc `[]`*(self: LocalStorage, key: string): Pon2Result[cstring] {.inline, noinit.} =
+  let key2 = key.pathPrefixAdded
+  if key2.hasItem:
+    ok key2.getItem
+  else:
+    err "key not found: {key}".fmt
+
+proc `[]=`*(self: var LocalStorage, key: string, val: cstring) {.inline, noinit.} =
+  key.pathPrefixAdded.setItem val
+
+proc del*(self: var LocalStorage, key: string) {.inline, noinit.} =
+  ## Deletes the key.
+  ## If the key is not contained in the local storage, does nothing.
+  key.pathPrefixAdded.removeItem
